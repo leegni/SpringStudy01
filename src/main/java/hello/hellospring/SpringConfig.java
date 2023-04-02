9c5ccfdc087787a5b673082b2b8b17c56e16ae25
Geunhee Lee <igni950424@gmail.com>
2023-04-02 19:10:50 +0900

    JPA 까지 230402

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,39 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
+    private DataSource dataSource;
+    private EntityManager em;
+    @Autowired
+    public SpringConfig(DataSource dataSource, EntityManager em, MemberRepository memberRepository){
+        this.dataSource = dataSource;
+        this.em = em;
+        this.memberRepository = memberRepository;
+    }
+
+
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        //return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+/*    @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
+        //return new JpaMemberRepository(em);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        //return new MemoryMemberRepository();
+    }*/
 }
